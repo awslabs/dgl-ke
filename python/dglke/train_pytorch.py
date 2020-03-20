ef7e51667a7c366a454c7f92c191f1b84dc08fa9
aksnzhy <mctt90@gmail.com>
2020-03-20 20:20:05 +0800

    update

--- python/dglke/train_pytorch.py
+++ python/dglke/train_pytorch.py
@@ -345,8 +345,11 @@ def dist_train_test(args, model, train_sampler, entity_pb, relation_pb, l2g, ran
             
             for metric in logs[0].keys():
                 metrics[metric] = sum([log[metric] for log in logs]) / len(logs)
+
+            print("-------------- Test result --------------")
             for k, v in metrics.items():
                 print('Test average {} : {}'.format(k, v))
+            print("-----------------------------------------")
 
             for proc in procs:
                 proc.join()
