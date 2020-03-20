68ac5f229ea7aafc2a4e806a0c9c42919527de77
Ubuntu <ubuntu@ip-172-31-23-90.us-east-2.compute.internal>
2020-03-20 08:43:03 +0000

    fix.

--- python/dglke/train.py
+++ python/dglke/train.py
@@ -112,10 +112,10 @@ def main():
     if args.neg_deg_sample_eval:
         assert not args.eval_filter, "if negative sampling based on degree, we can't filter positive edges."
 
+    args.soft_rel_part = args.mix_cpu_gpu and args.rel_part
     train_data = TrainDataset(dataset, args, ranks=args.num_proc)
     # if there is no cross partition relaiton, we fall back to strict_rel_part
     args.strict_rel_part = args.mix_cpu_gpu and (train_data.cross_part == False)
-    args.soft_rel_part = args.mix_cpu_gpu and args.rel_part and train_data.cross_part
     args.num_workers = 8 # fix num_worker to 8
 
     if args.num_proc > 1:
