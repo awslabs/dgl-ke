40826eb5e9f2636a23c23dcd3e974a1776f88ffa
aksnzhy <mctt90@gmail.com>
2020-03-20 18:57:15 +0800

    update

--- python/dglke/kvclient.py
+++ python/dglke/kvclient.py
@@ -121,8 +121,9 @@ def start_client(args, logger):
     init_time_start = time.time()
     time.sleep(WAIT_TIME) # wait for launch script
 
-    args.rel_part = False # We don't use relation partition in distributed training yet
     args.gpu = [-1] # We cannot support gpu distributed training yet
+    args.mix_cpu_gpu = False
+    args.rel_part = False # We don't use relation partition in distributed training yet
     args.total_machine = get_machine_count(args.ip_config)
     server_namebook = dgl.contrib.read_ip_config(filename=args.ip_config)
 
