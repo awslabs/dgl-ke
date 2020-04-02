c38726fa3d35837d891083e42c2cbe914447a7a6
Song <classicxsong@gmail.com>
2020-04-02 14:34:58 +0800

    Fix

--- tests/scripts/task_kg_test.sh
+++ tests/scripts/task_kg_test.sh
@@ -47,12 +47,11 @@ if [ "$2" == "cpu" ]; then
     dglke_train --model DistMult --dataset FB15k --batch_size 128 \
         --neg_sample_size 16 --hidden_dim 100 --gamma 500.0 --lr 0.1 --max_step 100 \
         --batch_size_eval 16 --valid --test -adv --eval_interval 30 --eval_percent 0.01 \
-        --save_emb DistMult_FB15k_emb --data_path /data/kg || fail "run DistMult on $2"
+        --data_path /data/kg || fail "run DistMult on $2"
 
     # verify saving training result
     python3 eval.py --model_name DistMult --dataset FB15k --hidden_dim 100 \
-        --gamma 500.0 --batch_size 16 --model_path DistMult_FB15k_emb/ \
-        --eval_percent 0.01 --data_path /data/kg || fail "eval DistMult on $2"
+        --gamma 500.0 --batch_size 16 --eval_percent 0.01 --data_path /data/kg || fail "eval DistMult on $2"
 elif [ "$2" == "gpu" ]; then
     # verify GPU training DistMult
     dglke_train --model DistMult --dataset FB15k --batch_size 128 \
@@ -64,12 +63,11 @@ elif [ "$2" == "gpu" ]; then
     dglke_train --model DistMult --dataset FB15k --batch_size 128 \
         --neg_sample_size 16 --hidden_dim 100 --gamma 500.0 --lr 0.1 --max_step 100 \
         --batch_size_eval 16 --gpu 0 --valid --test -adv --mix_cpu_gpu --eval_percent 0.01 \
-        --save_emb DistMult_FB15k_emb --data_path /data/kg || fail "run mix with async CPU/GPU DistMult"
+        --data_path /data/kg || fail "run mix with async CPU/GPU DistMult"
 
     # verify saving training result
     dglke_eval --model_name DistMult --dataset FB15k --hidden_dim 100 \
-        --gamma 500.0 --batch_size 16 --gpu 0 --model_path DistMult_FB15k_emb/ \
-        --eval_percent 0.01 --data_path /data/kg || fail "eval DistMult on $2"
+        --gamma 500.0 --batch_size 16 --gpu 0 --eval_percent 0.01 --data_path /data/kg || fail "eval DistMult on $2"
 
     if [ "$1" == "pytorch" ]; then
         # verify mixed CPU GPU training with async_update
@@ -96,12 +94,12 @@ elif [ "$2" == "gpu" ]; then
         dglke_train --model TransR --dataset FB15k --batch_size 128 \
             --neg_sample_size 16 --hidden_dim 100 --gamma 500.0 --lr 0.1 --max_step 100 \
             --batch_size_eval 16 --num_proc 2 --gpu 0 --valid --test -adv --eval_interval 30 \
-            --eval_percent 0.01 --data_path /data/kg --mix_cpu_gpu --rel_part --async_update \
-            --save_emb TransR_FB15k_emb || fail "run multiprocess TransR on $2"
+            --eval_percent 0.01 --data_path /data/kg --mix_cpu_gpu --rel_part \
+            --async_update || fail "run multiprocess TransR on $2"
 
         dglke_eval --model_name TransR --dataset FB15k --hidden_dim 100 \
-            --gamma 500.0 --batch_size 16 --num_proc 2 --gpu 0 --model_path TransR_FB15k_emb/ \
-            --eval_percent 0.01 --mix_cpu_gpu --data_path /data/kg || fail "eval multiprocess TransR on $2"
+            --gamma 500.0 --batch_size 16 --num_proc 2 --gpu 0 --eval_percent 0.01 \
+            --mix_cpu_gpu --data_path /data/kg || fail "eval multiprocess TransR on $2"
     fi
 fi
 
