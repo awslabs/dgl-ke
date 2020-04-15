0a15680164aadbf0ff0f6857f0d1c4ed4c284075
aksnzhy <mctt90@gmail.com>
2020-04-15 20:26:13 +0800

    update

--- python/dglke/train_pytorch.py
+++ python/dglke/train_pytorch.py
@@ -376,4 +376,6 @@ def dist_train_test(args, model, train_sampler, entity_pb, relation_pb, l2g, ran
             for proc in procs:
                 proc.join()
 
+            print('testing takes {:.3f} seconds'.format(time.time() - start))
+
         client.shut_down() # shut down kvserver
